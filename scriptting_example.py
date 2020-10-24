0f65b64a7ededad890bea5f5ede99da35bc4f8a3
truptimr <trupti.ranka@gmail.com>
2020-10-23 22:11:12 -0700

    changes to plot function

--- scriptting_example.py
+++ scriptting_example.py
@@ -3,11 +3,14 @@ from inputs import *
 import numpy as np
 from error_calculator import Error_calculator
 import matplotlib.pyplot as plt
-Nref = np.arange(3, 100, dtype=int)
+Nref = np.arange(1, 100, dtype=int)
 c=0
 Nref.shape
 error = np.zeros(Nref.shape)
 field['NSci'] = 1
+field['Nfield'] = 1
+field['rsep'] = 15
+field['rdref'] = 15
 for k in Nref:
 	field['Nref'] = k
 	output = Error_calculator(global_inputs,field,sigma_sci,sigma_NGS,RefObjNCatErr,'Absolute Astrometry')
@@ -15,5 +18,5 @@ for k in Nref:
 	c=c+1
 plt.plot(Nref,error)
 plt.xlabel('Number of reference stars',fontsize=14)
-plt.ylabel('arcseconds',fontsize=14)
+plt.ylabel('astrometry error ($\mu as$)',fontsize=14)
 plt.show()
