16d7e57fb99a92524034e717e44a91f099e2f1a0
truptimr <trupti.ranka@gmail.com>
2020-12-31 13:16:36 -0800

    final changes 2020.

--- test_2Dinterpolation.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from scipy import interpolate
-arr_Zbb=np.loadtxt('Zbb.csv',delimiter=',')
-f = interpolate.interp2d(arr_Zbb[:,0],arr_Zbb[:,1],arr_Zbb[:,2], kind='cubic')
-SNR = f(20,100)
